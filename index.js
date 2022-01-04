9d66d67ca77dcd63f1ac215042e3145e87b7a67e
xaveerarif <xaveerarif@gmail.com>
2022-01-05 00:42:45 +0600

    add service post method

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.get(cors());
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vea3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -16,6 +17,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const database = client.db("house_rent");
+    const rentDatabase = database.collection("rent");
+    // post api add house rent
+    app.post("/rent", async (req, res) => {
+      const servieces = req.body;
+      const result = await rentDatabase.insertOne(servieces);
+      console.log("hited", result);
+      res.json(result);
+    });
   } finally {
     // await client.close()
   }
@@ -27,5 +37,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Lisning from http://localhost:${port}/`);
+  console.log(`Lisning from http://localhost:${port}`);
 });
