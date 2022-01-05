16b786e9a5cd1a98c5f53c4ba40bff46a6fd6c0b
Mehedi Alam <mehedialam109@gmail.com>
2022-01-06 03:07:34 +0600

    add house updated

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const { application } = require("express");
+const ObjectId = require("mongodb").ObjectId;
+const fileUpload = require("express-fileupload");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vea3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -22,17 +25,40 @@ async function run() {
     const rentDatabase = database.collection("rent");
 
     //get api for rent
-    app.get("/rent", async (req, res) => {
+    app.get("/houses", async (req, res) => {
       const cursor = rentDatabase.find({});
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    // post api of houses
+    app.post("/addHouse", async (req, res) => {
+      console.log(req.body);
+      const country = req.body.country;
+      const city = req.body.city;
+      const rent = req.body.rent;
+      const description = req.body.description;
+      const image = req.files.image;
+      const picData = image.data;
+      const encodedPic = picData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPic, "base64");
+      const services = {
+        country,
+        city,
+        rent,
+        description,
+        image: imageBuffer,
+      };
+      const result = await rentDatabase.insertOne(services);
+      console.log(result);
+      res.json(result);
     });
 
     // delete api of houses
     app.delete("/houses/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await rentDatabaseCollection.deleteOne(query);
+      const result = await rentDatabase.deleteOne(query);
       res.send(result);
     });
 
